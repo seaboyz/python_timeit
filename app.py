32f4e55656c00ce223800ff0ab00773932224f59
seaboyz <seaboyz@hotmail.com>
2022-08-29 14:10:25 -0500

    timeit

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-input_list = range(100)
+import timeit
 
-def div_by_five(num):
-    if num % 5 == 0:
-        return True
-    else:
-        return False
-
-for num in input_list:
-    if div_by_five(num):
-        print(num)
+print(timeit.timeit("[x for x in range(100)]", number=1000000))
