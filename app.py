57cb492a251cca65b2c28339ac8fb77c6e963d0d
seaboyz <seaboyz@hotmail.com>
2022-08-29 14:23:45 -0500

    Generator vs list

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import timeit
 
+# Generator function
 print(timeit.timeit('''
 input_list = range(100)
 
@@ -9,6 +10,18 @@ def div_by_five(num):
     else:
         return False
 
+xyz = (i for i in input_list if div_by_five(i))
+''', number=500000))
 
-xyz = (i for i in range(1, 1000000) if div_by_five(i))
+# List Comprehension
+print(timeit.timeit('''
+input_list = range(100)
+
+def div_by_five(num):
+    if num % 5 == 0:
+        return True
+    else:
+        return False
+
+xyz = [i for i in input_list if div_by_five(i)]
 ''', number=500000))
